0be9d3ad4d62fd12afe50f8ab0f700244327da3e
矢頭春香 <haruyatoh@M-yatouharukanoMacBook-Pro.local>
2017-02-22 17:32:15 +0900

    Initial Commit

---
+++ JsonSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JsonSample
+//
+//  Created by 矢頭春香 on 2017/02/22.
+//  Copyright © 2017年 矢頭春香. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
